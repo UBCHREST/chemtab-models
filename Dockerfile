a76d7b88bf2b76beac0264a1beb32ec37c630e72
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-06-20 18:17:48 -0400

    updated tests (#1)

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,16 @@ FROM $ABLATE_DEPENDENCY_IMAGE
 COPY . /source-chemtab
 WORKDIR /build-chemtab
 
+# Setup the petsc env for tests
+ARG PETSC_BUILD_ARCH=arch-ablate-opt
+ENV PETSC_ARCH=$PETSC_BUILD_ARCH
+ENV PKG_CONFIG_PATH="${PETSC_DIR}/${PETSC_ARCH}/lib/pkgconfig:$PKG_CONFIG_PATH"
+ENV PATH="${PETSC_DIR}/${PETSC_ARCH}/bin:$PATH"
+
 # Configure & build
-run cmake -DCMAKE_BUILD_TYPE=Release -S /source-chemtab/tests -B .
-run make -j $(nproc)
+RUN cmake -DCMAKE_BUILD_TYPE=Release -S /source-chemtab/tests -B .
+RUN make -j $(nproc)
 
 # Specify Entry Point for tests
 ENV CTEST_OUTPUT_ON_FAILURE=ON
-CMD bash -c "echo 'Running Tests for ChemTab' && ctest "
+CMD bash -c "echo 'Running Tests for ChemTab' && make format-check-chemtab && ctest -L 'chemTabModelTests' "
---
+++ tests/clangFormatter.cmake
@@ -0,0 +1,39 @@
+# Download the clang format script
+include(FetchContent)
+FetchContent_Declare(
+        run-clang-format
+        GIT_REPOSITORY https://github.com/Sarcasm/run-clang-format.git
+        GIT_TAG 39081c9c42768ab5e8321127a7494ad1647c6a2f
+)
+FetchContent_MakeAvailable(run-clang-format)
+
+# Add a target to allow linting check
+find_program(CLANG_FORMAT "clang-format")
+find_package(Python)
+
+# Special case for versions less than 17
+if (CLANG_FORMAT AND Python_Interpreter_FOUND)
+    # determine the version
+    EXECUTE_PROCESS(COMMAND ${CLANG_FORMAT} --version OUTPUT_VARIABLE CLANG_FORMAT_INFO)
+    STRING(REGEX MATCH "([0-9]+).([0-9]+).([0-9]+)" CLANG_FORMAT_VERSION ${CLANG_FORMAT_INFO})
+
+    # Set the style file based upon the file, first the default
+    SET(CLANG_FORMAT_STYLE "--style=file")
+
+    # Special case for versions less than 17
+    if (${CLANG_FORMAT_VERSION} LESS 17)
+        SET(CLANG_FORMAT_STYLE "--style=file:.clang-format.v16")
+    endif ()
+
+    add_custom_target(
+            format-check-chemtab
+            COMMAND ${Python_EXECUTABLE} ${run-clang-format_SOURCE_DIR}/run-clang-format.py
+            ${CLANG_FORMAT_STYLE}
+            -r
+            --extensions=cpp,hpp,cc,hh,c++,h++,cxx,hxx,c,h
+            ${PROJECT_SOURCE_DIR}
+            --exclude '${PROJECT_SOURCE_DIR}/cmake-*'
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+            USES_TERMINAL
+    )
+endif ()
